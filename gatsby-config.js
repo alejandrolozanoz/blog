99ca57db8243d6e2f6a3e368cef27393b781f62f
Alejandro Lozano Zárate <alejandrolozanoz96@gmail.com>
2019-10-10 10:10:52 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Vitau | Blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Salud, nutrición, hábitos, cuidado personal y más maneras de ser la mejor versión de nosotros mismos.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
