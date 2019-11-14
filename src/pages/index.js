e75b9493473b86744f0300e88ffabb7b0c27663d
aka-cronos <jorge.escamilla89@gmail.com>
2019-11-13 19:26:11 -0600

    Create new index and categories wip page

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeLayout from '../layouts/HomeLayout'
+
+export default function index() {
+  return (
+    <HomeLayout>
+      <main className='main'>
+        <h1 className='title'>Tu salud es nuestra prioridad</h1>
+        <h2>Salud, nutrición, hábitos, cuidado personal y más maneras de ser la mejor versión de nosotros mismos.</h2>
+      </main>
+    </HomeLayout>
+  )
+}
