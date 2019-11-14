ff78e6a4076566a1dd169b51922ffbaa61a56dae
aka-cronos <jorge.escamilla89@gmail.com>
2019-11-14 14:41:43 -0600

    Wip Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import HomeLayout from '../layouts/HomeLayout'
+import BlogRoll from '../components/BlogRoll'
 
 export default function index() {
   return (
     <HomeLayout>
-      <main className='main'>
-        <h1 className='title'>Tu salud es nuestra prioridad</h1>
-        <h2>Salud, nutrición, hábitos, cuidado personal y más maneras de ser la mejor versión de nosotros mismos.</h2>
+      <main className="main">
+        <h1 className="title">Tu salud es nuestra prioridad</h1>
+        <h2>
+          Salud, nutrición, hábitos, cuidado personal y más maneras de ser la
+          mejor versión de nosotros mismos.
+        </h2>
       </main>
+
+      <section>
+        <BlogRoll />
+      </section>
     </HomeLayout>
   )
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,29 +3,11 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-// import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
+export const IndexPageTemplate = ({ title, description }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
+    <div className="full-width-image margin-top-0">
       <div
         style={{
           display: 'flex',
@@ -39,8 +21,7 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              '00aaff 0.5rem 0px 0px, 00aaff -0.5rem 0px 0px',
+            boxShadow: '00aaff 0.5rem 0px 0px, 00aaff -0.5rem 0px 0px',
             backgroundColor: '00aaff',
             color: 'white',
             lineHeight: '1',
@@ -49,19 +30,6 @@ export const IndexPageTemplate = ({
         >
           {title}
         </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              '00aaff 0.5rem 0px 0px, 00aaff -0.5rem 0px 0px',
-            backgroundColor: '00aaff',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
       </div>
     </div>
     <section className="section">
@@ -75,15 +43,8 @@ export const IndexPageTemplate = ({
 )
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
   description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
 }
 
 const IndexPage = ({ data }) => {
@@ -92,13 +53,8 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
         title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
         description={frontmatter.description}
-        intro={frontmatter.intro}
       />
     </Layout>
   )
@@ -119,34 +75,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
         description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
