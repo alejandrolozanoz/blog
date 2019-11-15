47cbda708490d0c9998eec1d60ad7b6a77fb0393
aka-cronos <jorge.escamilla89@gmail.com>
2019-11-14 23:35:59 -0600

    Add footer, fix header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,27 @@
 import React from 'react'
-import {Link} from 'gatsby'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faInstagram,
-  faFacebookSquare
-} from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'gatsby'
+import SocialMedia from '../components/SocialMedia'
 
 export default function Header() {
   return (
     <header className='header'>
-      <a className='header-logo' href="/">
-        <img src="/img/vitau-logo.svg" alt="Vitau"/>
+      <a className='header-logo' href='/'>
+        <img src='/img/vitau-logo.svg' alt='Vitau' />
       </a>
 
       <nav className='mainNav'>
-        <Link to="/" className="mainNav-link" activeClassName='isActive'>Inicio</Link>
-        <Link to="/categorias" className="mainNav-link" activeClassName='isActive'>Categorías</Link>
+        <Link to='/' className='mainNav-link' activeClassName='isActive'>
+          Inicio
+        </Link>
+        <Link
+          to='/categorias'
+          className='mainNav-link'
+          activeClassName='isActive'
+        >
+          Categorías
+        </Link>
 
-        <a className='mainNav-socialIcon' target="_blank" rel="noopener noreferrer" href="https://instagram.com/vitaumedical">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a className='mainNav-socialIcon' target="_blank" rel="noopener noreferrer" href="https://facebook.com/vitaumedical">
-          <FontAwesomeIcon icon={faFacebookSquare} />
-        </a>
+        <SocialMedia />
       </nav>
     </header>
   )
--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import useSiteMetadata from '../components/SiteMetadata'
 
 import '../styles/app.scss'
@@ -19,6 +20,8 @@ export default function HomeLayout({ children }) {
       <Header />
 
       {children}
+
+      <Footer />
     </>
   )
 }
