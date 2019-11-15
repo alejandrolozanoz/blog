1e3914e292afebb56dcad86970a7f537b1fea00f
aka-cronos <jorge.escamilla89@gmail.com>
2019-11-15 14:33:58 -0600

    Wip post preview and post template

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import usePostsInfo from './PostsInfo'
+import { Link } from 'gatsby'
 
 export default function PostFeed() {
   return (
-    <section className='postFeed'>
-      <h3 className='title'>Artículos más recientes</h3>
+    <section className="postFeed">
+      <h3 className="title">Artículos más recientes</h3>
 
       {usePostsInfo().map((postInfo, key) => {
-        const { title, description } = postInfo.node.frontmatter
+        const {
+          fields: { slug },
+          frontmatter: { title, description, featuredimage },
+        } = postInfo.node
 
         return (
-          <article className='postPreview'>
-            <h4 key={key}>{title}</h4>
-            <p>{description}</p>
-          </article>
+          <Link to={slug}>
+            <article className="postPreview">
+              <h4 key={key}>{title}</h4>
+
+              <img
+                src={featuredimage && featuredimage.childImageSharp.fluid.src}
+                alt=""
+              />
+            </article>
+          </Link>
         )
       })}
     </section>
--- src/components/PostsInfo.js
+++ src/components/PostsInfo.js
@@ -4,14 +4,25 @@ const usePostsInfo = () => {
   const { allMarkdownRemark } = useStaticQuery(
     graphql`
       query ALL_MARKDOWN_QUERY {
-        allMarkdownRemark {
+        allMarkdownRemark(sort: { order: ASC, fields: frontmatter___date }) {
           edges {
             node {
               id
+              fields {
+                slug
+              }
               frontmatter {
                 title
                 featuredpost
                 description
+                date(formatString: "MMMM DD, YYYY")
+                featuredimage {
+                  childImageSharp {
+                    fluid(maxWidth: 120, quality: 100) {
+                      src
+                    }
+                  }
+                }
               }
             }
           }
