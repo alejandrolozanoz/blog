576b45a922d929f64d23a519c799d180e220c76c
aka-cronos <jorge.escamilla89@gmail.com>
2019-11-16 02:49:21 -0600

    Wip content styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,42 +1,83 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 import HomeLayout from '../layouts/HomeLayout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const BlogPostTemplate = ({
+  content,
+  contentComponent,
+  tags,
+  title,
+  helmet,
+  date,
+  featuredimage,
+  html,
+}) => {
+  const PostContent = contentComponent || Content
+
+  return (
+    <article className='blogPost'>
+      {helmet || ''}
+      <header className='blogPost-header'>
+        <h1 className='title title--big'>{title}</h1>
+
+        <div className='blogPost-headerDetails'>
+          <span>{date}</span> | <span>{tags[0]}</span>
+        </div>
+      </header>
+
+      {featuredimage && (
+        <figure className='blogPost-featureImage'>
+          <img src={featuredimage.childImageSharp.fluid.src} alt='' />
+        </figure>
+      )}
+
+      <PostContent content={content} />
+    </article>
+  )
+}
+
+BlogPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
 
 const BlogPost = ({ data }) => {
   const {
-    frontmatter: { title, description, date, tags, featuredimage },
+    frontmatter: { title, description, tags, date, featuredimage },
     html,
   } = data.markdownRemark
 
   return (
     <HomeLayout>
-      <Helmet>
-        <title>{title} | Blog</title>
-        <meta name='description' content={description} />
-      </Helmet>
-      <article className='blogPost'>
-        <header className='blogPost-header'>
-          <h1 className='title title--big'>{title}</h1>
-
-          <div className='blogPost-headerDetails'>
-            <span>{date}</span> | <span>{tags[0]}</span>
-          </div>
-        </header>
-
-        {featuredimage && (
-          <figure className='blogPost-featureImage'>
-            <img src={featuredimage.childImageSharp.fluid.src} alt='' />
-          </figure>
-        )}
-
-        <div
-          className='blogPost-content'
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </article>
+      <BlogPostTemplate
+        content={html}
+        contentComponent={HTMLContent}
+        description={description}
+        helmet={
+          <Helmet titleTemplate='%s | Blog'>
+            <title>{title}</title>
+            <meta name='description' content={description} />
+          </Helmet>
+        }
+        tags={tags}
+        title={title}
+        date={date}
+        featuredimage={featuredimage}
+      />
     </HomeLayout>
   )
+}
+
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
 }
 
 export default BlogPost
@@ -47,7 +88,7 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
-        date(formatString: "DD MMMM, YYYY")
+        date(formatString: "MMMM DD, YYYY")
         title
         description
         tags
